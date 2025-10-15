2f83d472c29480251d6eb0447f4800a27c438f1e
Paweł <pawel@msd.info.pl>
2025-10-15 14:51:40 +0200

    Kontekst

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public Product(string name, decimal price, int stock)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+        public override string ToString()
+        {
+            return $"{Name} - Price: {Price:C}, Stock: {Stock}";
+        }
+    }
+}
---
+++ ConsoleApp/ProductsMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ProductsMethods
+    {
+
+        public Product CreateNewProduct()
+        {
+            Console.Write("Enter product name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter product price: ");
+            decimal price;
+            while (!decimal.TryParse(Console.ReadLine(), out price) || price < 0)
+            {
+                Console.Write("Invalid input. Please enter a valid price: ");
+            }
+            Console.Write("Enter product stock: ");
+            int stock;
+            while (!int.TryParse(Console.ReadLine(), out stock) || stock < 0)
+            {
+                Console.Write("Invalid input. Please enter a valid stock quantity: ");
+            }
+            return new Product(name, price, stock);
+        }
+
+
+    }
+}
